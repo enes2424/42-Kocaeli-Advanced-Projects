37b07a69f5159ca8018176f4426b062cf45d133c
Enes Mahmut Ates <eates@k2m17s02.42kocaeli.com.tr>
2025-10-13 12:58:29 +0300

    malloc

--- src/alloc.c
+++ src/alloc.c
@@ -4,13 +4,13 @@
 t_memory_management g_mm = {0};
 pthread_mutex_t g_memory_lock = PTHREAD_MUTEX_INITIALIZER;
 
-size_t  get_page_size(void)
+size_t get_page_size(void)
 {
     static size_t pagesize = 0;
 
     if (pagesize == 0)
     {
-        long    ps = sysconf(_SC_PAGESIZE);
+        long ps = sysconf(_SC_PAGESIZE);
 
         if (ps == -1)
             ps = 4096;
@@ -21,22 +21,21 @@ size_t  get_page_size(void)
     return pagesize;
 }
 
-
-size_t  get_zone_size_tiny(void) /* N */
+size_t get_zone_size_tiny(void) /* N */
 {
     return 16 * get_page_size();
 }
 
-size_t  get_zone_size_small(void) /* M */
+size_t get_zone_size_small(void) /* M */
 {
     return 128 * get_page_size();
 }
 
-size_t  get_max_size_tiny_malloc(void) /* n */
+size_t get_max_size_tiny_malloc(void) /* n */
 {
     size_t N = get_zone_size_tiny();
 
-    if (N < (100 * (sizeof(t_block) + 1) + sizeof(t_zone)))
+    if (N < (100 * (sizeof(t_block) + sizeof(size_t)) + sizeof(t_zone)))
         return 0;
 
     size_t n = (N - sizeof(t_zone)) / 100 - sizeof(t_block);
@@ -47,10 +46,14 @@ size_t  get_max_size_tiny_malloc(void) /* n */
     return n;
 }
 
-size_t  get_max_size_small_malloc(void) /* m */
+size_t get_max_size_small_malloc(void) /* m */
 {
     size_t M = get_zone_size_small();
     size_t n = get_max_size_tiny_malloc();
+    size_t alignment = 2 * sizeof(size_t);
+
+    if ((n + 1 + sizeof(size_t)) % alignment != 0)
+        n += alignment - ((n + 1 + sizeof(size_t)) % alignment);
 
     if (M < (100 * (sizeof(t_block) + n + 1) + sizeof(t_zone)))
         return 0;
